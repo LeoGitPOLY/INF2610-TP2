09aa6d216583d181f8adfaa3ebce796fbfbd64a9
Aurélie  Nichols <aurelie.nichols@polymtl.ca>
2023-09-26 15:52:17 -0400

    Question 1.2

--- part1.c
+++ part1.c
@@ -11,20 +11,22 @@
 // TODO
 // Si besoin, ajouter ici les directives d'inclusion
 // -------------------------------------------------
-
 // -------------------------------------------------
 
 void question1()
 {
     printf("Level 0\n");
+    registerProc(getpid(), getppid(), 0, 0);
 
     if (fork() == 0)
     {
         printf("Level 1.1\n");
+        registerProc(getpid(), getppid(), 1, 1);
 
         if (fork() == 0)
         {
             printf("Level 2.1\n");
+            registerProc(getpid(), getppid(), 2, 1);
             _exit(0);
         }
 
@@ -36,16 +38,19 @@ void question1()
     if (fork() == 0)
     {
         printf("Level 1.2\n");
+        registerProc(getpid(), getppid(), 1, 2);
 
         if (fork() == 0)
         {
             printf("Level 2.2\n");
+            registerProc(getpid(), getppid(), 2, 2);
             _exit(0);
         }
         wait(NULL);
         if (fork() == 0)
         {
             printf("Level 2.3\n");
+            registerProc(getpid(), getppid(), 2, 3);
             _exit(0);
         }
         wait(NULL);
@@ -56,22 +61,26 @@ void question1()
     if (fork() == 0)
     {
         printf("Level 1.3\n");
+        registerProc(getpid(), getppid(), 1, 3);
 
         if (fork() == 0)
         {
             printf("Level 2.4\n");
+            registerProc(getpid(), getppid(), 2, 4);
             _exit(0);
         }
         wait(NULL);
         if (fork() == 0)
         {
             printf("Level 2.5\n");
+            registerProc(getpid(), getppid(), 2, 5);
             _exit(0);
         }
         wait(NULL);
         if (fork() == 0)
         {
             printf("Level 2.6\n");
+            registerProc(getpid(), getppid(), 2, 6);
             _exit(0);
         }
 
@@ -80,4 +89,5 @@ void question1()
     }
 
     wait(NULL);
+    printProcRegistrations();
 }
