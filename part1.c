f4c4b5b5bad3da2b79e74718e829cf2f211b68bb
Leonard Pouliot <leonard.pouliot@polymtl.com>
2023-09-26 15:30:16 -0400

    question 1.1

--- part1.c
+++ part1.c
@@ -3,7 +3,7 @@
  *
  * Ecole polytechnique de Montreal, GIGL, Automne  2022
  * vos noms, prénoms et matricules
-*/
+ */
 
 #include "libprocesslab/libprocesslab.h"
 
@@ -15,7 +15,68 @@
 
 void question1()
 {
-   
-    // TODO
-}
+    printf("Level 0\n");
 
+    if (fork() == 0)
+    {
+        printf("Level 1.1\n");
+
+        if (fork() == 0)
+        {
+            printf("Level 2.1\n");
+            _exit(0);
+        }
+
+        wait(NULL);
+        _exit(0);
+    }
+    wait(NULL);
+    
+    if (fork() == 0)
+    {
+        printf("Level 1.2\n");
+
+        if (fork() == 0)
+        {
+            printf("Level 2.2\n");
+            _exit(0);
+        }
+        wait(NULL);
+        if (fork() == 0)
+        {
+            printf("Level 2.3\n");
+            _exit(0);
+        }
+        wait(NULL);
+        _exit(0);
+    }
+    wait(NULL);
+
+    if (fork() == 0)
+    {
+        printf("Level 1.3\n");
+
+        if (fork() == 0)
+        {
+            printf("Level 2.4\n");
+            _exit(0);
+        }
+        wait(NULL);
+        if (fork() == 0)
+        {
+            printf("Level 2.5\n");
+            _exit(0);
+        }
+        wait(NULL);
+        if (fork() == 0)
+        {
+            printf("Level 2.6\n");
+            _exit(0);
+        }
+
+        wait(NULL);
+        _exit(0);
+    }
+
+    wait(NULL);
+}
