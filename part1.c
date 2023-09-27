53b64d3d49bb81e459d18936d7d0fc102e976848
Leonard Pouliot <leonard.pouliot@polymtl.com>
2023-09-26 23:55:35 -0400

    Question 2.1

--- part1.c
+++ part1.c
@@ -13,90 +13,89 @@
 // -------------------------------------------------
 // -------------------------------------------------
 
-int counter()
+int CountAllChild()
 {
     int status;
     int childCount = 0;
+
     while (wait(&status) > 0)
     {
         childCount++;
     }
     return childCount;
 }
+
 void question1()
 {
-    printf("Level 0\n");
+    // Level 0
     registerProc(getpid(), getppid(), 0, 0);
 
     if (fork() == 0)
     {
-        printf("Level 1.1\n");
+        // Level 1.1
         registerProc(getpid(), getppid(), 1, 1);
 
         if (fork() == 0)
         {
-            printf("Level 2.1\n");
+            // Level 2.1
             registerProc(getpid(), getppid(), 2, 1);
             _exit(0);
         }
-        counter();
-        wait(NULL);
+        CountAllChild();
         _exit(0);
     }
     wait(NULL);
 
     if (fork() == 0)
     {
-        printf("Level 1.2\n");
+        // Level 1.2
         registerProc(getpid(), getppid(), 1, 2);
 
         if (fork() == 0)
         {
-            printf("Level 2.2\n");
+            // Level 2.2
             registerProc(getpid(), getppid(), 2, 2);
             _exit(0);
         }
         if (fork() == 0)
         {
-            printf("Level 2.3\n");
+            // Level 2.3
             registerProc(getpid(), getppid(), 2, 3);
             _exit(0);
         }
-        counter();
-        wait(NULL);
+        CountAllChild();
         _exit(0);
     }
     wait(NULL);
 
     if (fork() == 0)
     {
-        printf("Level 1.3\n");
+        // Level 1.3
         registerProc(getpid(), getppid(), 1, 3);
 
         if (fork() == 0)
         {
-            printf("Level 2.4\n");
+            // Level 2.4
             registerProc(getpid(), getppid(), 2, 4);
             _exit(0);
         }
         if (fork() == 0)
         {
-            printf("Level 2.5\n");
+            // Level 2.5
             registerProc(getpid(), getppid(), 2, 5);
             _exit(0);
         }
         if (fork() == 0)
         {
-            printf("Level 2.6\n");
+            // Level 2.6
             registerProc(getpid(), getppid(), 2, 6);
             _exit(0);
         }
-        counter();
-        wait(NULL);
+        CountAllChild();
         _exit(0);
     }
-    
-    printf("Number of children: %d\n", counter());
+
+    printf("Number of children: %d\n", CountAllChild());
     printProcRegistrations();
     execl("/usr/bin/ls", "ls", "-l", NULL);
 }
--- part2.c
+++ part2.c
@@ -3,7 +3,7 @@
  *
  * Ecole polytechnique de Montreal, GIGL, Automne  2022
  * vos noms, prénoms et matricules
-*/
+ */
 
 // TODO
 // Si besoin, ajouter ici les directives d'inclusion
@@ -19,21 +19,46 @@
 // nb est le nombre de threads qui vont contribuer au calcul
 #define nb 4
 
-//tableau somme est utilisé pour le calcul des sommes patielles par les threads
+// tableau somme est utilisé pour le calcul des sommes patielles par les threads
 long somme[nb];
 
-
 // fonction exécutée par chaque thread créé
-void* contribution(void*p)
+void *contribution(void *p)
 {
-    // TODO
+  printf("Thread %d de tid=%lu \n", *((int *)p), pthread_self());
+  int indexThread = *((int *)p);
+  long startingValue = indexThread * m / nb + 1;
+  long endingValue = (indexThread + 1) * m / nb;
+  long result = 0;
+
+  for (long i = startingValue; i <= endingValue; i++)
+  {
+    result += i;
+  }
+
+  somme[indexThread] = result;
   return NULL;
 }
 
-
-void question2( )
+void question2()
 {
-    // TODO
-    
-}
+  pthread_t *tid = malloc(nb * sizeof(pthread_t));
+  int num[4] = {0, 1, 2, 3};
 
+  for (int i = 0; i < nb; i++)
+  {
+    pthread_create(&tid[i], NULL, contribution, (void *)&num[i]);
+  }
+
+  long sommeTot = 0;
+  long wantedResult = m * (long)(m + 1) / 2;
+
+  for (int i = 0; i < nb; i++)
+  {
+    pthread_join(tid[i], NULL);
+    sommeTot += somme[i];
+  }
+
+  printf("somme[0] + ... + somme[nb − 1] = %ld \n", sommeTot);
+  printf("m ∗ (m + 1)/2 = %ld \n", wantedResult);
+}
